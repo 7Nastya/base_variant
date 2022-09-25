a9e7a2cad5fd7f8bfe2f27bd21697370ac299f72
Nastya_7 <saa_1299@mail.ru>
2022-09-25 22:38:14 +0300

    Add requirements.txt

--- excel_import_and_export.py
+++ excel_import_and_export.py
@@ -10,32 +10,35 @@ def export_file(database_name, output_file_name):
     connection = sqlite3.connect(database_name)
     c = connection.cursor()
     mysel = c.execute('''SELECT u.Id, u.Second_name, u.First_name, u.Patronymic, r.Region_name, c.City_name, u.Phone, u.Email 
-                 FROM users u
-                 INNER JOIN regions r ON r.Id = u.Region_id 
-                 INNER JOIN cities c ON c.Region_id = u.Region_id AND c.Id = u.City_id''')
+                         FROM users u
+                         INNER JOIN regions r ON r.Id = u.Region_id 
+                         INNER JOIN cities c ON c.Region_id = u.Region_id AND c.Id = u.City_id''')
     for i, row in enumerate(mysel):
         for j, value in enumerate(row):
             worksheet.write(i, j, row[j])
     workbook.close()
+    # сохраняем изменения
+    connection.commit()
+    connection.close()
 
 
 def input_file(database_name, input_file_name):
     connection = sqlite3.connect(database_name)
-    c = connection.cursor()
+    cursor = connection.cursor()
     file_to_read = openpyxl.load_workbook(input_file_name, data_only=True)
     sheet = file_to_read['Sheet1']
-    c.execute('SELECT * FROM users')
+    cursor.execute('SELECT * FROM users')
     users = []
-    for row in c:
+    for row in cursor:
         users.append(list(row))
     dict_region = {}
-    c.execute('SELECT Id, Region_name FROM regions')
-    for row in c:
+    cursor.execute('SELECT Id, Region_name FROM regions')
+    for row in cursor:
         dict_region[row[1]] = row[0]
 
     dict_city = {}
-    c.execute('SELECT Id, City_name FROM cities')
-    for row in c:
+    cursor.execute('SELECT Id, City_name FROM cities')
+    for row in cursor:
         dict_city[row[1]] = row[0]
     for row in range(1, sheet.max_row + 1):
         # Объявление списка
@@ -51,7 +54,8 @@ def input_file(database_name, input_file_name):
                 data[5] = dict_city[data[5]]
                 if data not in users:
                     Update_Table = """INSERT OR REPLACE INTO users VALUES(?, ?, ?, ?, ?, ?, ?, ?);"""
-                    c.execute(Update_Table, (data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]))
+                    cursor.execute(Update_Table,
+                                   (data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]))
 
     # сохраняем изменения
     connection.commit()
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from app.database_create import check_db
-from app.excel_import_and_export import export_file, input_file
+from db_create_and_check import check_db
+from excel_import_and_export import export_file, input_file
 
 database_name = 'tests.db'
 # Проверка базы данных
 check_db(database_name)
 
 # экспорт файла
-output_file_name = 'test_file'
+output_file_name = 'export.xlsx'
 export_file(database_name, output_file_name)
 
 #импорт файла
-input_file_name = 'test.xlsx'
+input_file_name = 'import.xlsx'
 input_file(database_name, input_file_name)
