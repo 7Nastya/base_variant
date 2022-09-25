69668aa1aaed16aa2c40a5cde89a97e7e87ec9a9
Nastya_7 <saa_1299@mail.ru>
2022-09-25 21:55:26 +0300

    Функционал приложения: 1. Возможность импортировать данные из xls/xlsx в таблицу users. При импорте название региона и города должны быть импортированы в виде id. 2. Возможность экспорта данных из таблицы users в xlsx При экспорте id региона и города должны быть представлены в виде наименований.

---
+++ app/excel_import_and_export.py
@@ -0,0 +1,58 @@
+import sqlite3
+from xlsxwriter.workbook import Workbook
+import openpyxl
+
+
+def export_file(database_name, output_file_name):
+    workbook = Workbook(output_file_name)
+    worksheet = workbook.add_worksheet()
+
+    connection = sqlite3.connect(database_name)
+    c = connection.cursor()
+    mysel = c.execute('''SELECT u.Id, u.Second_name, u.First_name, u.Patronymic, r.Region_name, c.City_name, u.Phone, u.Email 
+                 FROM users u
+                 INNER JOIN regions r ON r.Id = u.Region_id 
+                 INNER JOIN cities c ON c.Region_id = u.Region_id AND c.Id = u.City_id''')
+    for i, row in enumerate(mysel):
+        for j, value in enumerate(row):
+            worksheet.write(i, j, row[j])
+    workbook.close()
+
+
+def input_file(database_name, input_file_name):
+    connection = sqlite3.connect(database_name)
+    c = connection.cursor()
+    file_to_read = openpyxl.load_workbook(input_file_name, data_only=True)
+    sheet = file_to_read['Sheet1']
+    c.execute('SELECT * FROM users')
+    users = []
+    for row in c:
+        users.append(list(row))
+    dict_region = {}
+    c.execute('SELECT Id, Region_name FROM regions')
+    for row in c:
+        dict_region[row[1]] = row[0]
+
+    dict_city = {}
+    c.execute('SELECT Id, City_name FROM cities')
+    for row in c:
+        dict_city[row[1]] = row[0]
+    for row in range(1, sheet.max_row + 1):
+        # Объявление списка
+        data = []
+        # Цикл по столбцам от 1 до 8 ( 9 не включая)
+        for col in range(1, 9):
+            value = sheet.cell(row, col).value
+            # Список который мы потом будем добавлять
+            data.append(value)
+            number_of_elements = len(data)
+            if number_of_elements == 8:
+                data[4] = dict_region[data[4]]
+                data[5] = dict_city[data[5]]
+                if data not in users:
+                    Update_Table = """INSERT OR REPLACE INTO users VALUES(?, ?, ?, ?, ?, ?, ?, ?);"""
+                    c.execute(Update_Table, (data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]))
+
+    # сохраняем изменения
+    connection.commit()
+    connection.close()
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from app.bd import check_db
+from app.database_create import check_db
+from app.excel_import_and_export import export_file, input_file
 
+database_name = 'tests.db'
+# Проверка базы данных
+check_db(database_name)
 
-datebase_name = 'tests.db'
-check_db(datebase_name)
-#создание строк в бдimports
-#
+# экспорт файла
+output_file_name = 'test_file'
+export_file(database_name, output_file_name)
 
+#импорт файла
+input_file_name = 'test.xlsx'
+input_file(database_name, input_file_name)
